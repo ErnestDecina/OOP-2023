442b4070f625a2c20d499e6055e798994930cf36
Ernest Decina <ernestjohn007@gmail.com>
2023-01-25 16:39:15 +0000

    Updated Main.java && added Cat.java

---
+++ src/ie/tudublin/Cat.java
@@ -0,0 +1,18 @@
+package ie.tudublin;
+
+public class Cat {
+    // Fields
+    String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void speak() {
+        System.out.println("Meow");
+    }
+}
--- src/ie/tudublin/Main.java
+++ src/ie/tudublin/Main.java
@@ -3,5 +3,14 @@ package ie.tudublin;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-    } // End void main
-} // End class Main
+
+        Cat tara = new Cat();
+
+        tara.setName("Tara");
+
+        System.out.println(tara.getName());
+
+        tara.speak();
+
+    } // Emd main
+} // End Main
