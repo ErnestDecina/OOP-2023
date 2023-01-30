b99a77fff4e6dd68ca428fb776c71d11a45d3bc8
Ernest Decina <ernestjohn007@gmail.com>
2023-01-30 12:41:07 +0000

    Created code to draw all seeing eye

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -1,17 +1,21 @@
 package ie.tudublin;
 
+import com.jogamp.nativewindow.util.Rectangle;
+
 import processing.core.PApplet;
 
 public class HelloProcessing extends PApplet
 {
+	int screenWidth = 500;
+	int screenHeight = 500;
 
 	public void settings()
 	{
-		size(500, 500);
+		size(screenWidth, screenHeight);
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		// colorMode(HSB);
 		background(0);
 
 		x1 = random(0, width);
@@ -36,6 +40,41 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
+		background(255, 0, 0);
 
-	}
+		// Circle
+		stroke(0);
+		strokeWeight(0);
+		fill(255, 255, 0);
+		circle(screenWidth / 2, screenHeight / 2, 425);
+
+		// Triangle
+		stroke(0);
+		strokeWeight(0);
+		fill(0, 0, 255);
+		triangle(screenWidth / 2, 0, 10, screenHeight - 80, 490, screenHeight - 80);
+		
+		// Eye
+		stroke(0);
+		strokeWeight(0);
+		fill(255, 255, 255);
+
+		if (mousePressed) {
+			ellipse((screenWidth / 2), (screenHeight / 2) - 40, 0, 0);
+		}
+		else {
+			ellipse((screenWidth / 2), (screenHeight / 2) - 40, 230, 100);	
+		}
+		
+		// Eye Pupil
+		stroke(0);
+		strokeWeight(0);
+		fill(0, 0, 0);
+		if (mousePressed) {
+			circle((screenWidth / 2), (screenHeight / 2) - 40, 0);
+		}
+		else {
+			circle((screenWidth / 2), (screenHeight / 2) - 40, 50);	
+		}
+ 	}
 }
